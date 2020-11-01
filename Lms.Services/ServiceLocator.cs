4b841166455e3c531811b61cd66b2fcfbee0e9ba
Conrad Bekondo <philllittle302@gmail.com>
2020-11-01 05:03:48 +0100

    created service locator

---
+++ Lms.Services/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lms.Services
+{
+    public delegate IServiceCollection ServiceInitializeSubscription(IServiceCollection serviceCollection);
+    public static class ServiceLocator
+    {
+        private static ServiceProvider _serviceProvider;
+        private static bool _initialized;
+        public static ServiceInitializeSubscription InitRegistry;
+
+        public static void Initialize(IServiceCollection services)
+        {
+            if (_initialized) return;
+            
+            if(services is null) throw new ArgumentNullException(nameof(services));
+            InitRegistry?.Invoke(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public static T Resolve<T>() => _serviceProvider.GetService<T>();
+    }
+}
