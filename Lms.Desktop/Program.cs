96daea8304f19fc63b2081eb6287cb4a6fcc247e
Conrad Bekondo <philllittle302@gmail.com>
2020-11-01 04:52:22 +0100

    added services and nuget packages

---
+++ Lms.Desktop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lms.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            
+        }
+    }
+}
